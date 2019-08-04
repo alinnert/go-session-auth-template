8848c4006d2a6689cd68f810ff60d1820c172b45
Andreas Linnert <andreas@linnertmedia.de>
2019-08-04 20:29:07 +0200

    Managing badger transactions manually
    Managing badger transactions manually results in
    cleaner code (imho)

--- services/badger.go
+++ services/badger.go
@@ -4,44 +4,41 @@ import "github.com/dgraph-io/badger"
 
 // StoreValues stores a map in the database
 func StoreValues(values map[string][]byte, db *badger.DB) error {
-	err := db.Update(func(txn *badger.Txn) error {
-		for key, val := range values {
-			err := txn.Set([]byte(key), val)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
+	txn := db.NewTransaction(true)
+	defer txn.Discard()
 
-	if err != nil {
+	for key, val := range values {
+		err := txn.Set([]byte(key), val)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := txn.Commit(); err != nil {
 		return err
 	}
+
 	return nil
 }
 
 // GetValue reads a value from the database
 func GetValue(key []byte, db *badger.DB) (string, error) {
-	var value string
+	txn := db.NewTransaction(false)
+	defer txn.Discard()
 
-	err := db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(key)
-		if err != nil {
-			return err
-		}
-
-		val, err := item.ValueCopy(nil)
-		if err != nil {
-			return err
-		}
-
-		value = string(val)
-
-		return nil
-	})
+	item, err := txn.Get(key)
 	if err != nil {
 		return "", err
 	}
 
-	return value, nil
+	value, err := item.ValueCopy(nil)
+	if err != nil {
+		return "", err
+	}
+
+	if err := txn.Commit(); err != nil {
+		return "", err
+	}
+
+	return string(value), nil
 }
