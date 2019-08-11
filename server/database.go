da039c11742b5e87e591e89d7a722806b876cdf9
Andreas Linnert <andreas@linnertmedia.de>
2019-08-12 00:13:38 +0200

    Add documented options for BadgerDB

--- server/database.go
+++ server/database.go
@@ -8,8 +8,16 @@ import (
 
 func database() *badger.DB {
 	badgerOptions := badger.DefaultOptions("data.db")
-	// This should only be used on devices with low RAM (e.g. Raspi)
+
+	// This option should only be used on devices with low RAM (e.g. Raspi)
 	// badgerOptions.ValueLogLoadingMode = options.FileIO
+
+	// This option should be used on Windows
+	// If the process crashes while the database is open,
+	// It needs truncate to "repair" the database.
+	// This is not an issue on Linux.
+	badgerOptions.Truncate = true
+
 	db, err := badger.Open(badgerOptions)
 	if err != nil {
 		log.Fatal(err)
